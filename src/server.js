2a3094b1914109e9d38aed9949713cef63c70a5d
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-11-04 14:47:46 +0100

    server file created

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import productRoute from "";
+import reviewRoute from "";
+import { testConnetion, connetDB } from "./db/index.js";
+
+const server = express();
+
+const { PORT } = process.env;
+
+server.use(express.json());
+
+server.use("/products", productRoute);
+server.use("/reviews", reviewRoute);
+
+console.table(listEndpoints(server));
+
+server.listen(PORT, async () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+  await testConnetion();
+  await connetDB();
+});
